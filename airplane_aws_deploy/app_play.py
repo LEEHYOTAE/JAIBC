d0de9a08ddbf3c249ae3418597a35e55c74a2985
이효태(HYOTAE LEE) <101856176+LEEHYOTAE@users.noreply.github.com>
2023-05-18 23:58:56 +0900

    Add files via upload

---
+++ airplane_aws_deploy/app_play.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import streamlit as st
+st.set_page_config(layout="wide")
+# st.set_page_config(page_title="CanAI chatbot (GPT-powered)", page_icon=":robot_face:")
+import app
+
+app.go()
+
